d4bfd8e57d8f7ee5dc1943b3e269a045c02d8ebc
Noloquideus <daniilmanukian@gmail.com>
2023-10-06 01:56:36 +0300

    feat: add function

--- src/extension.ts
+++ src/extension.ts
@@ -5,3 +5,17 @@ import * as fs from 'fs';
 interface CustomQuickPickItem extends vscode.QuickPickItem {
     extension: string;
 }
+
+export function activate(context: vscode.ExtensionContext) {
+    // Регистрация команды расширения
+    let disposable = vscode.commands.registerCommand('simple-create-file.showCreateFileMenu', () => {
+        // Создание элемента быстрого выбора
+        const quickPick = vscode.window.createQuickPick<CustomQuickPickItem>();
+
+        // Установка элементов выбора
+        quickPick.items = [
+            { label: 'Создать файл Python', extension: 'py' },
+            { label: 'Создать файл JSON', extension: 'json' }
+        ];
+	});
+}
